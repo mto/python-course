0d16ae8f171b42d310f6efae32949520d9282904
Minh Hoang TO <hoang281283@gmail.com>
2017-06-08 18:49:01 +0700

    Solution for ex1, ex2

---
+++ Session1/ex_1.py
@@ -0,0 +1,4 @@
+print('Minh Hoang TO')
+print(35)
+print('Engineer')
+print('hoang281283@gmail.com')
---
+++ Session1/ex_2.py
@@ -0,0 +1,7 @@
+def print_user(name, age, job, email):
+    print(name)
+    print(age)
+    print(job)
+    print(email)
+
+print_user('Minh Hoang TO', 35, 'Engineer', 'hoang281283@gmail.com')
