caca149ebd1a330e8aa9dfeed8476e3c630483b7
Minh Hoang TO <hoang281283@gmail.com>
2017-06-12 17:26:05 +0700

    Complete wanna_cry.py

---
+++ Session2/ex_8.py
@@ -0,0 +1,9 @@
+def encrypt(src, code):
+    pass
+
+
+def decrypt(src, code, paid=False):
+    if not paid:
+        return "Please pay first"
+    else:
+        pass
--- Session2/wanna_cry.py
+++ Session2/wanna_cry.py
@@ -1,8 +1,36 @@
-f = open('hanmactu.txt')
-dst = open('out.txt','w')
+import tkinter as tk
+import os
 
-line = f.readline()
+from ex_8 import encrypt, decrypt
 
-while line != '':
-    dst.write(line)
-    line = f.readline()
+code = '123456789'
+
+
+def victim_accept_paying():
+    for f in os.listdir('./test_wanna_cry'):
+        try:
+            decrypt(f, code, paid=True)
+        except Exception as ex:
+            pass
+
+
+def start_wanna_cry():
+    root = tk.Tk()
+
+    frame = tk.Frame(root)
+    frame.pack()
+
+    msgb = tk.Message(frame, text='Your data under test_wanna_cry is affected, click on PAY to fix this')
+    msgb.pack(side=tk.TOP)
+
+    pbtn = tk.Button(frame, text='PAY', command=victim_accept_paying)
+    pbtn.pack(side=tk.BOTTOM)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    for f in os.listdir('./test_wanna_cry'):
+        encrypt(f, code)
+
+    start_wanna_cry()
